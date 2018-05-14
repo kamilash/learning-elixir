31846a1c33249d398f72dfb72c2f55b52ceafd25
Kamila Hinckel <kahinckel@gmail.com>
2018-05-13 23:22:21 -0300

    removing not necessary tuple, fixing warnings

--- pattern_matching/pattern/lib/pattern.ex
+++ pattern_matching/pattern/lib/pattern.ex
@@ -2,14 +2,14 @@ defmodule Pattern do
     def swap_values({a,b}) do
         {b,a}
     end
-    def are_equal({a,a}) do
+    def are_equal(a,a) do
         true
     end
-    def are_equal({a,b}) do
+    def are_equal(_,_) do
         false
     end
 end
 
 IO.inspect Pattern.swap_values({1,2})
-IO.puts Pattern.are_equal({1,1})
-IO.puts Pattern.are_equal({1,2})
+IO.puts Pattern.are_equal(1,1)
+IO.puts Pattern.are_equal(1,2)
