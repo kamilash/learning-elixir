c1f61aad0ddc8994ff4df7f4d73791f3fd0f6a15
Kamila Hinckel <kahinckel@gmail.com>
2018-06-19 19:10:08 -0300

    some changes

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -11,4 +11,16 @@ defmodule Hangman.Game do
       }
     end
 
+  def make_move(game = %{ game_state: :won }, _guess) do
+    { game, tally(game) }
+  end
+
+  def make_move(game = %{ game_state: :lost }, _guess) do
+    { game, tally(game) }
+  end
+
+  def tally(game) do
+    123
+  end
+
 end
--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -4,11 +4,21 @@ defmodule GameTest do
   alias Hangman.Game
 
   test "new_game returns structure" do
-    
     game = Game.new_game()
     assert game.turns_left == 7
     assert game.game_state == :initializing
     assert length(game.letters) > 0
     Enum.map(game.letters, fn(x) -> assert x =~ ~r/[a-z]/ end)
   end
+
+  test  "state isn't changed for :won game" do
+    game = Game.new_game() |> Map.put(:game_state, :won)
+    assert { ^game, _ } = Game.make_move(game, "x")
+  end
+
+  test  "state isn't changed for :lost game" do
+    game = Game.new_game() |> Map.put(:game_state, :lost)
+    assert { ^game, _ } = Game.make_move(game, "x")
+  end
+
 end
